95c8e4c5ebf04c093137bb6141df0904784b41c6
Damien Chave <41973020+Damien-Chave@users.noreply.github.com>
2018-10-04 08:56:32 +0200

    Update title.js

--- title.js
+++ title.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	var headTitle = document.getElementsByTagName("title")[0];
-	var Title = footer.getElementsByTagName("title")[1];
+	var Title = document.getElementsByTagName("title")[1];
 	headTitle.innerHTML = "Damien Chave Coding Pages !";
   Title.innerHTML = "Home repository";
 };
