ae4612478df43cda265d6e30d3f5d914fc51c8cb
Damien Chave <41973020+Damien-Chave@users.noreply.github.com>
2018-10-04 09:29:02 +0200

    Update title.js

--- title.js
+++ title.js
@@ -2,9 +2,11 @@ window.onload = function() {
 	var headTitle = document.getElementsByTagName("title")[0];
 	var header = document.getElementsByTagName("header")[0];
 	var h1 = header.getElementsByTagName("h1")[0];
+	var para = header.getElementsByTagName("p")[0];
 	var github = header.getElementsByTagName("ul")[0];
 	
 	headTitle.innerHTML = "Damien Chave Coding Pages";
-  	h1.innerHTML = "Welcome to Damien Chave Coding Pages !";
-	github.delete();
+  	h1.innerHTML = "Welcome to Damien Chave Coding Pages";
+	//delete para;
+	delete github;
 };
